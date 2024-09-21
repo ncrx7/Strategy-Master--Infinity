6d4c6567677c124ea266608a5f653969cd11b9fa
ncrx7 <buysal31@gmail.com>
2024-09-21 16:01:33 +0300

    feat: Added enemy unit character spawner AI

--- Assets/Scripts/UnitCharacterSpawnOnSceneManager.cs
+++ Assets/Scripts/UnitCharacterSpawnOnSceneManager.cs
@@ -11,6 +11,7 @@ public class UnitCharacterSpawnOnSceneManager : MonoBehaviour
     [SerializeField] private int _enemyUnitCharacterSpawnInterval;
     private int _playerUnitNumberOnScene = 0;
     private bool _isCreating = false;
+    [SerializeField] private CharacterClassType _previousSpawnedUnit; //IT IS SerializeField TO SEE IN EDITOR
 
     private void OnEnable()
     {
@@ -24,7 +25,7 @@ public class UnitCharacterSpawnOnSceneManager : MonoBehaviour
 
     private void Start()
     {
-        // CreatingEnemyUnitCharacter();
+        CreatingEnemyUnitCharacterAI();
     }
 
     private async void HandleCreatingPlayerUnitCharacter(int index, CharacterClassType characterClassType)
@@ -53,7 +54,7 @@ public class UnitCharacterSpawnOnSceneManager : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             EventSystem.SetSliderBarValue?.Invoke(BarType.UNITCHARACTER_SPAWN_PROGRESS_BAR, Mathf.Clamp01(elapsedTime / spawnTime), 1, BaseType.PLAYER_BASE);
-            await UniTask.Yield(); 
+            await UniTask.Yield();
         }
 
         unitCharacterManager.gameObject.SetActive(true);
@@ -64,25 +65,44 @@ public class UnitCharacterSpawnOnSceneManager : MonoBehaviour
         _isCreating = false;
     }
 
-    private async void CreatingEnemyUnitCharacter()
+    private async void CreatingEnemyUnitCharacterAI()
     {
         await UniTask.Delay(1000);
 
         while (true)
         {
-
-            UnitCharacterManager enemyUnitCharacterManager = UnitCharacterPoolManager.Instance.GetUnitCharacter(CharacterClassType.MEELE_FIGHTER, CharacterOwnerType.ENEMY_UNIT);
+            CharacterClassType chosenType = ChooseUnitType();
+            UnitCharacterManager enemyUnitCharacterManager = UnitCharacterPoolManager.Instance.GetUnitCharacter(chosenType, CharacterOwnerType.ENEMY_UNIT);
 
             if (enemyUnitCharacterManager == null)
+            {
+                await UniTask.Delay(_enemyUnitCharacterSpawnInterval * 1000);
                 continue;
+            }
+
+            _previousSpawnedUnit = chosenType;
 
             await UniTask.Delay(enemyUnitCharacterManager.CurrentClassData.spawnTime * 1000);
-            enemyUnitCharacterManager.gameObject.SetActive(true);
-            Debug.Log("waited character spawn time");
 
+            enemyUnitCharacterManager.gameObject.SetActive(true);
 
             await UniTask.Delay(_enemyUnitCharacterSpawnInterval * 1000);
-            Debug.Log("waited interval : " + _enemyUnitCharacterSpawnInterval);
         }
+    }
+
+    private CharacterClassType ChooseUnitType()
+    {
+        if (_previousSpawnedUnit == CharacterClassType.MEELE_FIGHTER)
+        {
+            return (Random.value > 0.5f) ? CharacterClassType.MAGE : CharacterClassType.RIFLE;
+        }
+        else
+        {
+            int randomValue = Random.Range(0, 100);
+            if (randomValue < 40) return CharacterClassType.MEELE_FIGHTER;
+            if (randomValue < 65) return CharacterClassType.MAGE;        
+            if (randomValue < 85) return CharacterClassType.RIFLE;    
+            return CharacterClassType.HEALER;
+        }                  
     }
 }
